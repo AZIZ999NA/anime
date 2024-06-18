ea1fff8dbbd6a4f46e19dfec6547771f9b07a589
AZIZ999NA <ademmario50@gmail.com>
2024-06-19 00:57:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const changeImageBtn = document.getElementById("changeImageBtn");
     const heroSection = document.querySelector(".hero");
 
-    const originalBackgroundImage = "jujtsu.jpg"; // Original image
+    const originalBackgroundImage = "jujtsu1.jpg"; // Original image
     const newBackgroundImage = "imgg.jpg"; // New image
 
     let isOriginalImage = true;
